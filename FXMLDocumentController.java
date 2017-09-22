25dafa4d2fc424f8af343d0936d1ca28fb852c86
itsamiracle <canozbek1@yahoo.com>
2017-09-22 16:54:45 +0200

    Add files via upload

---
+++ FXMLDocumentController.java
@@ -0,0 +1,72 @@
+/*
+ * 
+ */
+package javafxml_mycalculator;
+
+import java.lang.*;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+/**
+ *
+ *
+ */
+public class FXMLDocumentController /*implements Initializable*/ {
+    
+    private double num1 ;
+    private boolean isOperationSet = false;
+    private String value1 = new String();
+    @FXML
+    private Button exit;
+    @FXML
+    private TextField output;
+    @FXML
+    private Text output2;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        if(event.getTarget() == exit)
+            Platform.exit();
+        
+        else if(isOperationSet == false){
+            value1 = value1 + ((Button)event.getSource()).getText();
+            output.setText(value1);
+            //System.out.println(value1);
+            return;
+        }
+        else
+            value1 = value1 + ((Button)event.getSource()).getText();
+            output.setText(value1);
+            //System.out.println(value1);
+            
+        }
+        
+    
+    @FXML
+    private void handleOperatorAction(ActionEvent event) {
+        
+        if (((Button)event.getSource()).getText().equals("C")){
+            output.setText("");
+            value1 = "";
+            isOperationSet = false;
+            return;
+        }
+        else if (((Button)event.getSource()).getText().equals("=")) {
+            num1 = stringtoCalc.eval(value1);
+            output.setText(String.valueOf(num1));
+            output2.setText(value1 + "=" + String.valueOf(num1));
+            value1 = String.valueOf(num1);
+            isOperationSet = false;
+            return;
+        }
+        
+        isOperationSet = true;
+        value1 = value1 + ((Button)event.getSource()).getText();
+        output.setText(value1);
+    }
+}
---
+++ JavaFXML_MyCalculator.java
@@ -0,0 +1,56 @@
+/*
+ * 
+ */
+package javafxml_mycalculator;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ *
+ * @author can
+ */
+public class JavaFXML_MyCalculator extends Application {
+    private double xOffset;
+    private double yOffset;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        stage.initStyle(StageStyle.TRANSPARENT);
+        
+        // this anonymous inner class expression can be turned into a lambda expression
+        root.setOnMousePressed(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event){
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+        // like this :
+        root.setOnMouseDragged(event -> {
+                stage.setX(event.getScreenX() - xOffset);
+                stage.setY(event.getScreenY() - yOffset);
+        });
+        
+        Scene scene = new Scene(root);
+        scene.setFill(javafx.scene.paint.Color.TRANSPARENT);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
